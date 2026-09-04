a8d3790a085c05921dbec9c52751a7e248d84e59
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2026-09-04 11:00:16 -0300

    init

---
+++ src/main/java/maraisaferreira/com/github/music_library_90s/MusicLibrary90sApplication.java
@@ -0,0 +1,13 @@
+package maraisaferreira.com.github.music_library_90s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicLibrary90sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicLibrary90sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maraisaferreira/com/github/music_library_90s/MusicLibrary90sApplicationTests.java
@@ -0,0 +1,13 @@
+package maraisaferreira.com.github.music_library_90s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicLibrary90sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
